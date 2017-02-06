c487aace54b58fd1ab34925c792090d065dd5d5c
Shahriar <Shahriar>
2017-02-06 09:50:01 +0600

    Assignment 6 February

--- movieQuery.js
+++ movieQuery.js
@@ -5,25 +5,54 @@ var movieList = [
   { title: "Gladiator", year: "2000", length: 155, rating: "8.5", genre: "historical" },
   { title: "The Patriot", year: "2000", length: 165, rating: "7.1", genre: "historical" },
   { title: "The Matrix Reloaded”, year: “2003", length: 138, rating: "7.2", genre: "sci-fi" }
+  {title: "Theory of Everything", year: "2014", length: 183, rating: "7.7", genre: "biography"} // added
 ];
 
 function getMovieTitle( index ){
-  //add code
+  return movieList[index].title;
 }
 
-console.log(getMovieTitle(3));
+console.log(getMovieTitle(3));  // works
 
 function addMovie( movie ){
   //add code
+  //var mov_title = prompt("Name of the movie?");
+  //var mov_year = prompt("Year of release?");
+  //var mov_length = prompt("Duration (in min)?");
+  //var mov_rating = prompt("Rating?");
+  //var mov_genre = prompt("Genre?");
+
+  //movieList.push(mov_title, mov_year, mov_length, mov_rating, mov_genre);
+  movieList.push(movie);
+
 }
 
-var newMovie = {};
-console.log(addMovie(newMovie));
+var newMovie = {"Dark Knight", "2008", 160, '8.0', 'thriller'};
+
+console.log(JSON.stringify(addMovie(newMovie)));
 
 function movieByRating(){
-  //add code
-}
+  var arr =[], arr2=[];
+  for (var i = 0; i < movieList.length; i++) {
+    arr.push(parseFloat(movieList[i].rating));
+  }
+  arr.sort();
+  //console.log(arr.length);
+  for (var i = 0; i < arr.length; i++){
 
+  console.log(arr[i]);
+  }
+for (var i = 0; i < arr.length; i++)
+{
+  for (var j = 0; j < arr.length; j++)
+  {
+    if (arr[i] == parseFloat(movieList[j].rating))
+    {
+      console.log(movieList[i]);
+    }
+  }
+}
+}
 console.log(movieByRating());
 
 function findByTitle( title ){
